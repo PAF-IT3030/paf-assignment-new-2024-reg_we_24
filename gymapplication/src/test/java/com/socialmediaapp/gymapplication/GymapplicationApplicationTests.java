841fdc38171cf3f0ee1fc0e4016c3ab75b69b148
Sathish Pedhuruarachchi <163474142+sathishpeduruarachchi@users.noreply.github.com>
2024-05-05 16:57:02 +0530

    befor repo deleting and new repo to added post ,management backend files

---
+++ gymapplication/src/test/java/com/socialmediaapp/gymapplication/GymapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmediaapp.gymapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
