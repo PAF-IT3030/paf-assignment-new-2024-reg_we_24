af1daeb37f2afea3e2457f5cbd96fe4e97ce3b73
IT21839160 <it21839160@my.sliit.lk>
2024-05-05 22:08:36 +0530

    Remove unused and modified files

--- src/main/java/com/fitness/social/media/platform/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fitness.social.media.platform.repository;
-
-import com.fitness.social.media.platform.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository extends JpaRepository<User,Long>{
-}
--- src/main/java/com/fitness/social/media/platform/repository/WorkoutRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fitness.social.media.platform.repository;
-
-import com.fitness.social.media.platform.model.Workoutinputs;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface WorkoutRepository extends JpaRepository<Workoutinputs,Long>{
-}
