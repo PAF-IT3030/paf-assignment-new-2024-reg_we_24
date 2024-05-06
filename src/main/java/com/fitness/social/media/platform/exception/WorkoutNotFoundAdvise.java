b5dc50dadbd50358bcb3545f1eae965de39b45e8
IT21839160 <it21839160@my.sliit.lk>
2024-05-06 17:49:21 +0530

    add WorkoutNotFound

--- src/main/java/com/fitness/social/media/platform/exception/WorkoutNotFoundAdvise.java
+++ src/main/java/com/fitness/social/media/platform/exception/WorkoutNotFoundAdvise.java
@@ -1,2 +1,16 @@
-package com.fitness.social.media.platform.exception;public class WorkoutNotFoundAdvise {
+package com.fitness.social.media.platform.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WorkoutNotFoundAdvise {
+    @ExceptionHandler(WorkoutFoundException.class)
+    public Map<String,String> exceptionHandler (WorkoutFoundException exception){
+        Map<String,String>  errorMap= new HashMap<>();
+        errorMap.put("errorMessage", exception.getMessage());
+        return errorMap;
+    }
+
 }
