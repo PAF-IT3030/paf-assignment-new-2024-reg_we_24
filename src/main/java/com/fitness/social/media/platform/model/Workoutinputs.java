ed18694e464c61ca2cc741c85eeba30143538a47
IT21839160 <it21839160@my.sliit.lk>
2024-05-06 15:55:48 +0530

    update workoutinputs

--- src/main/java/com/fitness/social/media/platform/model/Workoutinputs.java
+++ src/main/java/com/fitness/social/media/platform/model/Workoutinputs.java
@@ -9,112 +9,58 @@ public class Workoutinputs {
 @Id
 @GeneratedValue
 
-    private Long Id;
-    private String exname;
-    private String selection;
-    private String distance;
-    private String duration;
-    private String pace;
-    private String terrain;
-    private String weight;
-    private String sets;
-    private String reps;
-    private String style;
-    private String poses;
+    private Long id;
+    private String muscle;
+    private String name;
+    private String time;
+    private String sett;
+    private String image;
 
     public Long getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Long id) {
-        Id = id;
+        id = id;
     }
 
-    public String getExname() {
-        return exname;
+    public String getMuscle() {
+        return muscle;
     }
 
-    public void setExname(String exname) {
-        this.exname = exname;
+    public void setMuscle(String muscle) {
+        this.muscle = muscle;
     }
 
-    public String getSelection() {
-        return selection;
+    public String getName() {
+        return name;
     }
 
-    public void setSelection(String selection) {
-        this.selection = selection;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getDistance() {
-        return distance;
+    public String getTime() {
+        return time;
     }
 
-    public void setDistance(String distance) {
-        this.distance = distance;
+    public void setTime(String time) {
+        this.time = time;
     }
 
-    public String getDuration() {
-        return duration;
+    public String getSett() {
+        return sett;
     }
 
-    public void setDuration(String duration) {
-        this.duration = duration;
+    public void setSett(String sett) {
+        this.sett = sett;
     }
 
-    public String getPace() {
-        return pace;
+    public String getImage() {
+        return image;
     }
 
-    public void setPace(String pace) {
-        this.pace = pace;
-    }
-
-    public String getTerrain() {
-        return terrain;
-    }
-
-    public void setTerrain(String terrain) {
-        this.terrain = terrain;
-    }
-
-    public String getWeight() {
-        return weight;
-    }
-
-    public void setWeight(String weight) {
-        this.weight = weight;
-    }
-
-    public String getSets() {
-        return sets;
-    }
-
-    public void setSets(String sets) {
-        this.sets = sets;
-    }
-
-    public String getReps() {
-        return reps;
-    }
-
-    public void setReps(String reps) {
-        this.reps = reps;
-    }
-
-    public String getStyle() {
-        return style;
-    }
-
-    public void setStyle(String style) {
-        this.style = style;
-    }
-
-    public String getPoses() {
-        return poses;
-    }
-
-    public void setPoses(String poses) {
-        this.poses = poses;
+    public void setImage(String image) {
+        this.image = image;
     }
 }
