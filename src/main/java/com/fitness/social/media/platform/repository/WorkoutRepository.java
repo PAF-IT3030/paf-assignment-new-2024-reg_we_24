a63765adcf5d0afd4fd7a9bb204f09f3e340b12f
IT21839160 <it21839160@my.sliit.lk>
2024-05-06 17:56:03 +0530

    change workoutinputs to workout

--- src/main/java/com/fitness/social/media/platform/repository/WorkoutRepository.java
+++ src/main/java/com/fitness/social/media/platform/repository/WorkoutRepository.java
@@ -1,6 +1,7 @@
 package com.fitness.social.media.platform.repository;
 
+import com.fitness.social.media.platform.model.Workout;
 import com.fitness.social.media.platform.model.Workoutinputs;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface WorkoutRepository extends JpaRepository<Workoutinputs,Long>{
+public interface WorkoutRepository extends JpaRepository<Workout,Long>{
 }
