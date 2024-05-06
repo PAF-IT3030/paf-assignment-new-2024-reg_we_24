9681b0912b68449e11490943adfe8c2acae3a4b5
IT21839160 <it21839160@my.sliit.lk>
2024-05-06 17:54:48 +0530

    create instead of workoutinputs

--- src/main/java/com/fitness/social/media/platform/model/Workout.java
+++ src/main/java/com/fitness/social/media/platform/model/Workout.java
@@ -1,2 +1,67 @@
-package com.fitness.social.media.platform.model;public class Workout {
+package com.fitness.social.media.platform.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Workout {
+
+    @Id
+    @GeneratedValue
+
+    private Long id;
+    private String muscle;
+    private String name;
+    private String time;
+    private String sett;
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMuscle() {
+        return muscle;
+    }
+
+    public void setMuscle(String muscle) {
+        this.muscle = muscle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getSett() {
+        return sett;
+    }
+
+    public void setSett(String sett) {
+        this.sett = sett;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
