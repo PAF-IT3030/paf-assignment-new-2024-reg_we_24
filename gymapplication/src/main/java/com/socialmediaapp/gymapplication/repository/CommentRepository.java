a311cc3a1c0eb6e5ef29e2d5e968b011c598a351
Sathish Pedhuruarachchi <163474142+sathishpeduruarachchi@users.noreply.github.com>
2024-05-06 17:49:20 +0530

    Interface declaration: CommentRepository extends JpaRepository<Comment, Long>, which means it inherits CRUD operations for Comment entities and Method declaration: findByPostId(Long postId) - Custom query method to find comments by post ID

--- gymapplication/src/main/java/com/socialmediaapp/gymapplication/repository/CommentRepository.java
+++ gymapplication/src/main/java/com/socialmediaapp/gymapplication/repository/CommentRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// Interface declaration: CommentRepository extends JpaRepository<Comment, Long>, which means it inherits CRUD operations for Comment entities
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    // Method declaration: findByPostId(Long postId) - Custom query method to find comments by post ID
     List<Comment> findByPostId(Long postId);
 }
