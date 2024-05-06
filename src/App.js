62ea5b65472f0088a90127feaa58dedcfa8bd714
IT21839160 <it21839160@my.sliit.lk>
2024-05-06 19:18:59 +0530

    remainings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-
 import './App.css';
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EditUser from './users/EditUser';
+import ViewUser from "./users/ViewUser";
+import Check from './users/Check';
+import AddWorkout from './users/AddWorkout';
+import WorkOutHome from './pages/WorkoutHome';
 import Navbar from './layouts/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Router>
+        <Navbar/>
+
+        <Routes>
+          <Route exact path="/" element={<WorkOutHome/>} />
+          <Route exact path="/adduser" element={<AddWorkout/>} />
+          <Route exact path="/edituser/:id" element={<EditUser/>}/>
+          <Route exact path="/viewuser/:id" element={<ViewUser />} />
+          <Route exact path="/check" element={<Check />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
