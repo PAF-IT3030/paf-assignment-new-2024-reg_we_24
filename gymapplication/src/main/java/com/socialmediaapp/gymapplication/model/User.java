044e18645ea2065149ebb5efceeaa9a56fc948a6
Sathish Pedhuruarachchi <163474142+sathishpeduruarachchi@users.noreply.github.com>
2024-05-06 16:11:56 +0530

    update uuser model class

---
+++ gymapplication/src/main/java/com/socialmediaapp/gymapplication/model/User.java
@@ -0,0 +1,38 @@
+package com.socialmediaapp.gymapplication.model;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    // Constructors, getters, and setters
+    public User() {
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
