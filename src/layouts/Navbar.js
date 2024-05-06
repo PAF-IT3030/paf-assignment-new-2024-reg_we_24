9bcde61d5b9ea1bd1c452cc1e79c4958457817de
IT21839160 <it21839160@my.sliit.lk>
2024-05-06 19:18:40 +0530

    nav bar

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,34 +1,19 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default class Navbar extends PureComponent {
-  render() {
-    return (
-      <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Food Pease</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
+    
+    <Link className='btn btn-outline-light' to="/adduser">Create New Plan</Link>
   </div>
 </nav>
-      </div>
-    )
-  }
+    </div>
+  )
 }
