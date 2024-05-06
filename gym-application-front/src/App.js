cc1229be626131a3875e39b8542f5e697dd35845
Sathish Pedhuruarachchi <163474142+sathishpeduruarachchi@users.noreply.github.com>
2024-05-06 20:26:07 +0530

    updatingfrontend code

--- gym-application-front/src/App.js
+++ gym-application-front/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ImageUpload from './components/imageUpload/ImageUpload';
+import ImageView from './components/imageView/ImageView'; // Import ImageView component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ImageUpload />} />
+        <Route path="/image/:id" element={<ImageView />} /> {/* Route to ImageView component */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
+
