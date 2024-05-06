c43eb5ef088a11f0e36112965beb0e8de2ad6013
Sathish Pedhuruarachchi <163474142+sathishpeduruarachchi@users.noreply.github.com>
2024-05-06 16:54:19 +0530

    updated by comment moddel

---
+++ gymapplication/src/main/java/com/socialmediaapp/gymapplication/model/Comment.java
@@ -0,0 +1,53 @@
+package com.socialmediaapp.gymapplication.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private Long postId;
+
+    private LocalDateTime createdAt;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
