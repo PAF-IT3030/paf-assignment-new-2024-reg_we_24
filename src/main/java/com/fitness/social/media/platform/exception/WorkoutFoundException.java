0e6d3a603a38960435621a35fbb3b55a05c0aeb4
IT21839160 <it21839160@my.sliit.lk>
2024-05-06 15:59:09 +0530

    add found exception

--- src/main/java/com/fitness/social/media/platform/exception/WorkoutFoundException.java
+++ src/main/java/com/fitness/social/media/platform/exception/WorkoutFoundException.java
@@ -1,2 +1,5 @@
-package com.fitness.social.media.platform.exception;public class WorkoutFoundException {
+package com.fitness.social.media.platform.exception;
+
+public class WorkoutFoundException extends RuntimeException{
+    public WorkoutFoundException(Long id){super("could not found the user with id"+ id);}
 }
